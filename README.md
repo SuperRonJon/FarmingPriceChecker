2f9e5aba6a4b3bdb7caa517e655e191e83811c63
pseute <pseut001@odu.edu>
2017-11-20 16:28:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple script to check the prices of the most commonly farmed herbs in runescape, and output the herb with the current most profit per seed.
+
+Prices are taken from the OSbuddy Exchange at https://rsbuddy.com/exchange
---
+++ farmingscript.py
@@ -0,0 +1,70 @@
+import requests
+
+url = "http://api.rsbuddy.com/grandExchange?a=guidePrice&i="
+max_profit = 0
+most = "none"
+
+
+def check_max(to_check, current_max):
+    if to_check.profit > current_max:
+        return True
+
+
+def check_herb(herb_name, number, seed_number):
+    global most
+    global max_profit
+    herb = Herb(herb_name, number, seed_number)
+    herb.calc_profit()
+    herb.print()
+    if check_max(herb, max_profit):
+        most = herb.name
+        max_profit = herb.profit
+
+
+class Herb:
+    name = "x"
+    num = "x"
+    seedNum = "x"
+    seedPrice = 0
+    price = 0
+    profit = 0
+
+    def __init__(self, the_name, n, sn):
+        self.name = the_name
+        self.num = n
+        self.seedNum = sn
+
+    def calc_profit(self):
+        full_url = url + str(self.num)
+        response = requests.get(full_url)
+        data = response.json()
+        self.price = data["overall"]
+
+        full_url = url + str(self.seedNum)
+        response = requests.get(full_url)
+        data = response.json()
+        self.seedPrice = data["overall"]
+
+        self.profit = (self.price*6.5) - self.seedPrice
+
+    def print(self):
+        print(self.name.upper())
+        print("\tBuy: ", self.seedPrice)
+        print("\tSell: ", self.price)
+        print("\tProfit: ", self.profit)
+
+
+#check herb with (name, herb id, seed id)
+check_herb("ranarr", 257, 5295)
+check_herb("torstol", 269, 5304)
+check_herb("snapdragon", 3000, 5300)
+check_herb("avantoe", 261, 5298)
+check_herb("dwarf weed", 267, 5303)
+check_herb("kwuarm", 263, 5299)
+check_herb("toadflax", 2998, 5296)
+check_herb("lantadyme", 2481, 5302)
+check_herb("cadantine", 265, 5301)
+check_herb("irit", 259, 5297)
+
+print("\nMost profit: ", most.upper(), "at", max_profit, "gp.")
+enter = input("Press ENTER to close...")
